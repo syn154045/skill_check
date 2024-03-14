6aea4a0131bbbe89ecc0d6fc9469e5ef6bafdc9d
ishiyu <distilledfields@gmail.com>
2024-03-14 14:39:04 +0900

    fix(vite.config.js): 画像が表示されていなかった不具合を修正

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host:   true,
     port:   5173,
-    origin: `https://0.0.0.0:5173` // assets request origin
+    origin: `http://0.0.0.0:5173` // assets request origin
   },
 })
